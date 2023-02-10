e5277c11a24c51b151fa3b6a7dc66c7b9dba8910
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2023-02-10 17:51:31 -0300

    .

--- aula04/cliente/index.js
+++ aula04/cliente/index.js
@@ -1,20 +1,134 @@
-let tarefas = [];
+// API
+const BASE_URL = 'http://localhost:3000/tarefas';
+
+const cadastrarTarefa = async (tarefa) => {
+  try {
+    // Faz a requisição para a api
+    const response = await fetch(BASE_URL, {
+      // Definido o metodo para criar itens
+      method: 'POST',
+      // Convertido objeto JS para JSON e envia no corpo da requisição
+      body: JSON.stringify(tarefa),
+      // Cria headers para enviar na requisição, definindo o tipo de dado trafegado
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+    });
+
+    // Converte o retorno da api para um objeto/array
+    const data = await response.json();
+    console.log('Sucesso!', data);
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+};
+
+const obterTarefas = async (filtro = '') => {
+  try {
+    // Faz a requisição para a api
+    const response = await fetch(`${BASE_URL}${filtro}`);
+
+    // Converte o retorno da api para um objeto/array
+    const data = await response.json();
+
+    // Executa função para renderizar as tarefas no html
+    listaTarefas(data);
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+};
+
+const atualizarStatustarefa = async (id, status) => {
+  try {
+    // Faz a requisição para a api
+    const response = await fetch(`${BASE_URL}/${id}`, {
+      method: 'PATCH',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({
+        concluida: status,
+      }),
+    });
+
+    // Converte o retorno da api para um objeto/array
+    const data = await response.json();
+    console.log('Sucesso:', data);
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+};
+
+const obterTarefa = async (id) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${id}`);
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const atualizarTarefa = async (tarefa) => {
+  try {
+    // Faz a requisição para a api
+    const response = await fetch(`${BASE_URL}/${tarefa.id}`, {
+      // Definido o metodo para criar itens
+      method: 'PUT',
+      // Convertido objeto JS para JSON e envia no corpo da requisição
+      body: JSON.stringify(tarefa),
+      // Cria headers para enviar na requisição, definindo o tipo de dado trafegado
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+    });
+
+    // Converte o retorno da api para um objeto/array
+    const data = await response.json();
+    console.log('Sucesso!', data);
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+};
+
+// Aplicação
 
 const formCadastro = document.getElementById('form-cadastro');
-formCadastro.addEventListener('submit', (event) => {
+formCadastro.addEventListener('submit', async (event) => {
   event.preventDefault();
 
+  const id = event.target.id.value;
+  const concluida = event.target.concluido.value;
+
   const tarefa = {
-    id: new Date().getTime(),
     titulo: event.target.titulo.value,
     prioridade: event.target.prioridade.value,
     descricao: event.target.descricao.value,
     concluida: false,
   };
 
-  tarefas.push(tarefa);
+  if (id) {
+    //Edição
+    const tarefaEditada = {
+      ...tarefa,
+      id,
+      concluida: concluida === 'true' ? true : false,
+    };
 
-  listaTarefas();
+    // Chamar a fução para atualizar no servidor
+    await atualizarTarefa(tarefaEditada);
+  } else {
+    //Novo cadatros
+    await cadastrarTarefa(tarefa);
+  }
+
+  formCadastro.reset();
+  document.getElementById('id').value = null;
+  document.getElementById('concluido').value = null;
+
+  await obterTarefas();
 });
 
 // Formatar prioridade de acordo com valor selecionado
@@ -37,8 +151,26 @@ const obterPrioridade = (idPrioridade) => {
   }
 };
 
+// Função para popular o formulário
+const popularForm = (tarefa) => {
+  const id = document.getElementById('id');
+  id.value = tarefa.id;
+
+  const concluido = document.getElementById('concluido');
+  concluido.value = tarefa.concluida;
+
+  const titulo = document.getElementById('titulo');
+  titulo.value = tarefa.titulo;
+
+  const prioridade = document.getElementById('prioridade');
+  prioridade.value = tarefa.prioridade;
+
+  const descricao = document.getElementById('descricao');
+  descricao.value = tarefa.descricao;
+};
+
 // Criar os elementos no html, recebe uma lista de tarefas, ou usa o valor default
-const listaTarefas = (listaTarefas = tarefas) => {
+const listaTarefas = (listaTarefas) => {
   // Obtem a lista (ul) do html
   const lista = document.getElementById('lista');
   lista.innerHTML = '';
@@ -79,16 +211,26 @@ const listaTarefas = (listaTarefas = tarefas) => {
     checkbox.id = 'concluida';
     checkbox.classList.add('checkbox');
     checkbox.checked = tarefa.concluida;
-    checkbox.addEventListener('change', () => {
+    checkbox.addEventListener('change', async (event) => {
       // Atualizar apenas o status da tarefa clicada
-      const tarefasAtualizadas = tarefas.map((t) =>
-        t.id === tarefa.id ? { ...t, concluida: !t.concluida } : t
-      );
-      // Atualizar variavel de controle global
-      tarefas = tarefasAtualizadas;
-      console.log(tarefas);
+      const status = event.target.checked;
+      console.log('status', status);
+
+      // Chama método para atualizar o status na api
+      await atualizarStatustarefa(tarefa.id, status);
     });
     div.appendChild(checkbox);
+
+    // Criar botão de editar tarefa
+    const button = document.createElement('button');
+    button.innerText = 'Editar';
+    button.classList.add('button-primary');
+    button.addEventListener('click', async () => {
+      const data = await obterTarefa(tarefa.id);
+      popularForm(data);
+      console.log(data);
+    });
+    div.appendChild(button);
 
     // Adiciona a div no li
     listItem.appendChild(div);
@@ -105,9 +247,14 @@ butaoBusca.addEventListener('click', () => {
   // Obtem o valor digitado no input
   const valorBusca = document.getElementById('input-busca').value;
 
-  // Aplica o filtro
-  const tarefasFiltradas = tarefas.filter((tarefa) => tarefa.titulo.includes(valorBusca));
+  // Montando o filtro
+  const filtro = valorBusca ? `?titulo_like=${valorBusca}` : '';
 
-  // Cria os elementos filtrados no html
-  listaTarefas(tarefasFiltradas);
+  // Buscar tarefas filtradas
+  obterTarefas(filtro);
+});
+
+// Carregar lista de tarefas ao carregar a página
+window.addEventListener('load', async () => {
+  await obterTarefas();
 });
